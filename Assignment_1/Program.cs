73466dffedd967f709c8e6ba963a38157e792bdb
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2019-09-26 18:13:04 +0300

    6. Create and throw your own exception

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Linq;
 
 
 namespace Assignment_1
@@ -16,35 +17,24 @@ namespace Assignment_1
             
             string input = Console.ReadLine();
             var a = new RealTimeCityBikeDataFetcher();
-            a.GetBikeCountInStation(input);
+            if (input.Any(c => char.IsDigit(c)))
+            {
+                a.GetBikeCountInStation(input);
+            }
+            
+            
 
         }
     }
 
     class RealTimeCityBikeDataFetcher : ICityBikeDataFetcher
     {
-        static async Task Main()
-        {
-            try	
-            {
-                string api = "http://api.digitransit.fi/routing/v1/routers/hsl/bike_rental";
-                System.Net.HttpClient client = new System.Net.HttpClient();
-                HttpResponseMessage response = await client.GetAsync(api);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-
-                Console.WriteLine(responseBody);
-            }  
-            catch(HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught!");	
-                Console.WriteLine("Message :{0} ",e.Message);
-            }
-        }
+        
 
         public Task<int> GetBikeCountInStation(string stationName)
         {
-            throw new NotImplementedException();
+            
+            throw new NotImplementedException(String.Format("Invalid argument: {0} is not a number", stationName));
         }
     }
 }
