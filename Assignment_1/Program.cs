8ac542fcecb123d06dbec8a981e5357442f5f79d
Gideron <32290174+Gideron@users.noreply.github.com>
2019-09-26 16:37:06 +0300

    assignment 1 steps 1-4

---
+++ Assignment_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            RealTimeCityBikeDataFetcher();
+        }
+    }
+
+    public interface ICityBikeDataFetcher
+    {
+        Task<int> GetBikeCountInStation(string stationName);
+    }
+
+    class RealTimeCityBikeDataFetcher : ICityBikeDataFetcher
+    {
+        string api = "http://api.digitransit.fi/routing/v1/routers/hsl/bike_rental";
+        System.Net.HttpClient client = new System.Net.HttpClient();
+ 
+        static async Task Main()
+        {
+            try	
+            {
+                HttpResponseMessage response = await client.GetAsync(api);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseBody);
+            }  
+            catch(HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");	
+                Console.WriteLine("Message :{0} ",e.Message);
+            }
+            return 0;
+        }
+    }
+}
