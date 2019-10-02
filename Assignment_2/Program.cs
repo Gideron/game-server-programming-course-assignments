1d7665719935e8c00204ab3026fffad46783ac4f
Gideron <32290174+Gideron@users.noreply.github.com>
2019-10-02 20:29:40 +0300

    step 1

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -4,9 +4,24 @@ namespace Assignment_2
 {
     class Program
     {
+        public List<Player> players;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        private void instantiatePlayers(){
+            for(int i = 0; i < 1000000;i++){
+                //instantiate player with random Guid
+                Player p = new Player(Id = new Guid());
+                players.add(p);
+            }
+        }
+
+        private void checkDuplicatePlayers(){
+            foreach(Player p in players){
+            }
+            //or lambda?
         }
     }
 }
