c86ca78d715d1fc8f645e7778b903da38b46dfc1
Gideron <32290174+Gideron@users.noreply.github.com>
2019-10-02 20:14:04 +0300

    assignment 2 classes

---
+++ Assignment_2/Item.cs
@@ -0,0 +1,5 @@
+public class Item
+{
+    public Guid Id { get; set; }
+    public int Level { get; set; }
+}
---
+++ Assignment_2/Player.cs
@@ -0,0 +1,6 @@
+public class Player : IPlayer
+{
+    public Guid Id { get; set; }
+    public int Score { get; set; }
+    public List<Item> Items { get; set; }
+}
