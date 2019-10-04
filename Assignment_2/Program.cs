e354c4e722d6c99341ba225270a80dca3c3ed83b
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2019-10-04 17:59:58 +0300

    6. Lambda

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -81,12 +81,17 @@ namespace Assignment_2
                 return null;
         }
 
-        private static void ProcessEachItem(Player player, Action<Item> process)
+        //private static void ProcessEachItem(Player player, Action<Item> process)
+        //{
+        //    foreach(var item in player.Items)
+        //    {
+        //        process(item);
+        //    }
+        //}
+
+        private static void ProcessEachItemLambda(Player player, Action<Item> process)
         {
-            foreach(var item in player.Items)
-            {
-                process(item);
-            }
+            ProcessEachItemLambda(player, p => PrintItem(p));
         }
 
         private static void PrintItem(Item item){
