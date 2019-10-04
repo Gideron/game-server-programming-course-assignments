0ab73f454bb9892f3d8c7845c0021048c8fcede4
Gideron <32290174+Gideron@users.noreply.github.com>
2019-10-04 23:13:06 +0300

    Updated classes

--- Assignment_3/GameWebApi/FileRepository.cs
+++ Assignment_3/GameWebApi/FileRepository.cs
@@ -1,4 +1,24 @@
 public class FileRepository : IRepository {
     private string txt_file = "game-dev.txt";
     //File.ReadAllText  File.WriteAllText
+
+    public async Task<Player> Get(Guid id){
+        //return player with Guid
+    }
+
+    public async Task<Player[]> GetAll(){
+        //return all players
+    }
+
+    public async Task<Player> Create(Player player){
+        //create/write new player
+    }
+
+    public async Task<Player> Modify(Guid id, ModifiedPlayer player){
+        //update player with Guid
+    }
+    
+    public async Task<Player> Delete(Guid id){
+        //delete player with Guid
+    }
 }
--- Assignment_3/GameWebApi/PlayersController.cs
+++ Assignment_3/GameWebApi/PlayersController.cs
@@ -1,7 +1,52 @@
+[Route("api/[controller]")]
+[ApiController]
 public class PlayersController {
-    public Task<Player> Get(Guid id);
-    public Task<Player[]> GetAll();
-    public Task<Player> Create(NewPlayer player);
-    public Task<Player> Modify(Guid id, ModifiedPlayer player);
-    public Task<Player> Delete(Guid id);
+    private readonly ILogger<PlayersController> _logger;
+    private readonly IRepository _repository;
+
+    public PlayersController(ILogger<PlayersController> logger, IRepository repository)
+    {
+        _logger = logger;
+        _repository = repository;
+    }
+    
+    [HttpGet]
+    [Route("{playerId}")]
+    public Task<Player> Get(Guid id){
+        throw new NotImplementedException();
+    }
+
+    [HttpGet]
+    [Route()]
+    public Task<Player[]> GetAll(){
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Route("")]
+    [ValidateModel]
+    public Task<Player> Create(NewPlayer player){
+        throw new NotImplementedException();
+        /*_logger.LogInformation("Creating player with name " + newPlayer.Name);
+        var player = new Player()
+        {
+            Id = Guid.NewGuid(),
+            Name = newPlayer.Name,
+            CreationTime = DateTime.Now()
+        };
+        await _repository.CreatePlayer(player);
+        return player;*/
+    }
+
+    [HttpUpdate]
+    [Route("{playerId}")]
+    public Task<Player> Modify(Guid id, ModifiedPlayer player){
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete]
+    [Route("{playerId}")]
+    public Task<Player> Delete(Guid id){
+        throw new NotImplementedException();
+    }
 }
