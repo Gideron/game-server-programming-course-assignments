a88364175233298916fa02777bee1307c72fff62
Nico A-K <32290174+Gideron@users.noreply.github.com>
2019-10-08 19:10:16 +0300

    added a few validation attributes

--- Assignment_3/GameWebApi/Item.cs
+++ Assignment_3/GameWebApi/Item.cs
@@ -1,14 +1,19 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 public class Item
 {
     public enum ItemType {
         SWORD, POTION, SHIELD
     }
+
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public int Score { get; set; }
+
+    [Range(1, 99)]
     public int Level { get; set; }
+
+    [EnumDataType(typeof(ItemType))]
     public ItemType Type { get; set; }
+
     public DateTime CreationTime { get; set; }
 }
