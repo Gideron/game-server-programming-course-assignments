04cf4aa914822e7d9d51d938085496723ecd8756
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2019-10-09 14:39:43 +0300

    FileRepository added

---
+++ Project/api/FileRepository.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+
+namespace api
+{
+    public interface FileRepository : IRepository
+    {
+        private string txt_file = "game-score.txt";
+
+        Task<Player> IRepository.Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Player[]> IRepository.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Player> IRepository.Create(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Player> IRepository.Modify(string name, ModifiedPlayer player)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Player> IRepository.Delete(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
