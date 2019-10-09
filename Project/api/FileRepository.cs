6adb092799884a4ee99f3d3a2d81d7f059a3c7c0
Nico A-K <32290174+Gideron@users.noreply.github.com>
2019-10-09 17:10:05 +0300

    [WIP] FileRepository

--- Project/api/FileRepository.cs
+++ Project/api/FileRepository.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
 
 namespace api
 {
     public class FileRepository : IRepository
     {
-        private string txt_file = "game-score.txt";
+        private string filePath = "game-score.txt";
+        //File.ReadAllText  File.WriteAllText
+
         Task<Player> IRepository.Get(string name)
         {
             throw new NotImplementedException();
@@ -13,11 +18,30 @@ namespace api
 
         Task<Player[]> IRepository.GetAll()
         {
+            if (!File.Exists(filePath))
+            {
+                //file not found
+                throw new NotImplementedException();
+            }
+
+            string readText = File.ReadAllText(filePath);
+            Console.WriteLine(readText);
+            List<Player> pList = JsonUtility.FromJson<Player>(readText);
+            return pList.players;
+            
             throw new NotImplementedException();
         }
 
         Task<Player> IRepository.Create(Player player)
         {
+            /*if (!File.Exists(filePath))
+            {
+                //file not found
+                throw new NotImplementedException();
+            }
+            
+            string appendText = "This is extra text" + Environment.NewLine;
+            File.AppendAllText(filePath, appendText, Encoding.UTF8);*/
             throw new NotImplementedException();
         }
 
